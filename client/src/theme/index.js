9e7cab748d81ed6a58f792266f79ff8994fc7684
behan05 <=>
2025-06-14 19:18:24 +0530

    chore: added mui theme (typography, shape, palette, component, and breakpoint)

--- client/src/theme/index.js
+++ client/src/theme/index.js
@@ -1,5 +1,14 @@
-import { createTheme } from "@mui/material/styles"
+import { createTheme } from "@mui/material/styles";
+import { breakpoint } from "./breakpoints";
+import { components } from "./components";
+import { palette } from "./palette";
+import { shape } from "./shape";
+import { typography } from "./typography";
 
 export const theme = createTheme({
-
+    typography,
+    shape,
+    breakpoint,
+    components,
+    palette
 });
---
+++ client/src/theme/palette.js
@@ -0,0 +1,43 @@
+
+export const palette = {
+    mode: 'dark' || 'light',
+
+    primary: {
+        main: '#006D77', // Deep Peacock Teal
+        light: '#33a1a8',
+        dark: '#004f53',
+        contrastText: '#ffffff',
+    },
+    secondary: {
+        main: '#EF476F', // Saffron Coral
+        light: '#ff6f8e',
+        dark: '#b80043',
+        contrastText: '#ffffff',
+    },
+    text: {
+        primary: '#F5F5F5', // Off-white for readability
+        secondary: '#B0B0B0', // Muted gray
+        disabled: '#7a7a7a',
+    },
+    error: {
+        main: '#F43F5E', // Strong crimson pink (bold but unique)
+        contrastText: '#ffffff',
+    },
+    wraning: {
+        main: '#FFD166', // Gold glow
+        contrastText: '#000000',
+    },
+    info: {
+        main: '#3ABFF8', // Sky blue info
+        contrastText: '#ffffff',
+    },
+    success: {
+        main: '#10B981', // Soft lime teal (success with calm tone)
+        contrastText: '#ffffff',
+    },
+    background: {
+        default: '#0B0F10', // deep dark with bluish tone
+        paper: '#12181B', // slightly raised contrast
+    },
+    divider: 'rgba(255,255,255,0.08)',
+}
